9eaa93cfe870b0ffce807fd264720ad869696915
Filbert <frtsnts10@gmail.com>
2023-11-13 20:50:11 +0700

    pre-release version of portfolio

---
+++ script.js
@@ -0,0 +1,48 @@
+// function scrollToElement(elementSelector, instance = 0) {
+//   const elements = document.querySelectorAll(elementSelector);
+//   if (elements.length > instance) {
+//     elements[instance].scrollIntoView({ behavior: "smooth" });
+//   }
+// }
+
+// const link1 = document.getElementById("link1");
+// const link2 = document.getElementById("link2");
+// const link3 = document.getElementById("Link3");
+// const link4 = document.getElementById("Link4");
+
+// link1.addEventListener("click", () => {
+//   scrollToElement(".header");
+// });
+// link2.addEventListener("click", () => {
+//   scrollToElement(".header", 1);
+// });
+// link3.addEventListener("click", () => {
+//   scrollToElement(".column");
+// });
+// link4.addEventListener("click", () => {
+//   scrollToElement(".header");
+// });
+
+const toggleModeButton = document.getElementById('toggleModeButton');
+
+toggleModeButton.addEventListener('click', () => {
+  const bodyElement = document.body;
+  if (bodyElement.classList.contains('darkMode')) {
+    bodyElement.classList.remove('darkMode');
+  } else {
+    bodyElement.classList.add('darkMode');
+  }
+});
+
+const darkModeLogo = document.getElementById('darkModeLogo');
+const lightModeLogo = document.getElementById('lightModeLogo');
+
+toggleModeButton.addEventListener('click', () => {
+  if (document.body.classList.contains('darkMode')) {
+    darkModeLogo.style.display = 'block';
+    lightModeLogo.style.display = 'none';
+  } else {
+    darkModeLogo.style.display = 'none';
+    lightModeLogo.style.display = 'block';
+  }
+});
