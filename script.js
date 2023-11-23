5011cc286bf8a60527f537a6db55413d7b690d3b
Filbert <filbert1505@gmail.com>
2023-11-23 15:34:11 +0700

    Update 23/11
    - Dark mode PC fixed
    
    ToDo:
    - Fix dark mode mobile
    - Fix Typing Indicator
    - Fix Layout

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-// function scrollToElement(elementSelector, instance = 0) {
-//   const elements = document.querySelectorAll(elementSelector);
-//   if (elements.length > instance) {
-//     elements[instance].scrollIntoView({ behavior: "smooth" });
-//   }
-// }
-// window.addEventListener('DOMContentLoaded', function() {
-//   const link1 = document.getElementById("link1");
-//   const link2 = document.getElementById("link2");
-//   const link3 = document.getElementById("Link3");
-//   const link4 = document.getElementById("Link4");
-
-//   link1.addEventListener("click", () => {
-//     scrollToElement(".header");
-//   });
-//   link2.addEventListener("click", () => {
-//     scrollToElement(".header", 1);
-//   });
-//   link3.addEventListener("click", () => {
-//     scrollToElement(".column");
-//   });
-//   link4.addEventListener("click", () => {
-//     scrollToElement(".header");
-//   });
-// });
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
 
 
